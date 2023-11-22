e0e9137afe79f43c2e3f0e123814bf8cb7bab568
ahabuzaina <ah.abuzaina@hotmail.com>
2023-11-22 04:00:54 +0300

    Card iOS App using Storyboarding

---
+++ CardWorkout/CardSelectionVC.swift
@@ -0,0 +1,50 @@
+//
+//  CardSelectionVC.swift
+//  CardWorkout
+//
+//  Created by Ahmad Abuzaina on 11/21/23.
+//
+
+import UIKit
+
+class CardSelectionVC: UIViewController {
+
+    @IBOutlet var cardImageView: UIImageView!
+    @IBOutlet var buttons: [UIButton]!
+    
+    var cards: [UIImage] = Card.allValues
+    var timer: Timer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for button in buttons{
+            button.layer.cornerRadius       = 8
+        }
+        startTimer()
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(<#T##animated: Bool##Bool#>)
+        timer.invalidate()
+    }
+    
+    func startTimer(){
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(showRandomImage), userInfo: nil, repeats: true)
+    }
+    
+    @objc func showRandomImage(){
+        cardImageView.image = cards.randomElement() ?? UIImage(named: "AS")
+    }
+    
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
+    }
+    
+    @IBAction func restartButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
+        startTimer()
+    }
+
+}
